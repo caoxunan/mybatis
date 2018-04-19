9776edd56afe36506e4060b31d195cf0a7eef364
曹旭楠 <caoxunan@midaigroup.com>
2018-04-19 15:18:55 +0800

    mybatis通过动态代理获取Mapper实现类对象

---
+++ src/main/java/com/cxn/dao/UserMapper.java
@@ -0,0 +1,25 @@
+package com.cxn.dao;
+
+import com.cxn.model.User;
+
+import java.util.List;
+
+/**
+ * @program: mybatis
+ * @description: 验证mybatis动态代理实现接口的调用
+ * @author: cxn
+ * @create: 2018-04-19 15:06
+ * @Version v1.0
+ */
+public interface UserMapper {
+
+    User getById(Integer id);
+
+    List<User> queryAll();
+
+    void insertUser(User user);
+
+    void updateUser(User user);
+
+    void removeUser(Integer id);
+}
